68a9b9b7c46e50a18a5e6803eaec94750f11bf65
zarlent <alvdp@mail.ru>
2025-12-20 20:48:36 +0300

    Source.c

--- Source.c
+++ Source.c
@@ -7,7 +7,9 @@ double f(double x);
 double solve(double y, double a, double b, double eps);
 double der2(double x);
 void table(double a, double b, double step);
-void minmax(double a, double b, double step);
+void minmax(double a, double b, double step,
+            double* minx, double* minv,
+            double* maxx, double* maxv);
 
 int main() {
     setlocale(LC_ALL, "RUS");
@@ -19,9 +21,9 @@ int main() {
         printf("\n=== Меню ===\n");
         printf("1. Значение в точке\n");
         printf("2. Таблица значений\n");
-        printf("3. Минимум и максимум на отрезке\n");
+        printf("3. Минимум или максимум на отрезке\n");
         printf("4. Найти x по Y\n");
-        printf("5. Производная\n");
+        printf("5. Вторая производная\n");
         printf("0. Выход\n> ");
         scanf("%d", &choice);
 
@@ -34,31 +36,49 @@ int main() {
             break;
 
         case 2:
-            printf("a = "); 
+            printf("a = ");
             scanf("%lf", &a);
-            printf("b = "); 
+            printf("b = ");
             scanf("%lf", &b);
-            printf("step = "); 
+            printf("step = ");
             scanf("%lf", &step);
             table(a, b, step);
             break;
 
-        case 3:
-            printf("a = "); 
+        case 3: {
+            int mm;
+            double minx, minv, maxx, maxv;
+
+            printf("a = ");
             scanf("%lf", &a);
-            printf("b = "); 
+            printf("b = ");
             scanf("%lf", &b);
-            printf("step = "); 
+            printf("step = ");
             scanf("%lf", &step);
-            minmax(a, b, step);
+
+            printf("Что вывести?\n");
+            printf("1 - Минимум\n");
+            printf("2 - Максимум\n> ");
+            scanf("%d", &mm);
+
+            minmax(a, b, step, &minx, &minv, &maxx, &maxv);
+
+            if (mm == 1)
+                printf("\nМинимум: f(%.5f) = %.6f\n", minx, minv);
+            else if (mm == 2)
+                printf("\nМаксимум: f(%.5f) = %.6f\n", maxx, maxv);
+            else
+                printf("Неверный выбор\n");
+
             break;
+        }
 
         case 4:
             printf("Y = ");
             scanf("%lf", &y);
-            printf("a = "); 
+            printf("a = ");
             scanf("%lf", &a);
-            printf("b = "); 
+            printf("b = ");
             scanf("%lf", &b);
             printf("x ~= %.6f\n", solve(y, a, b, 1e-6));
             break;
@@ -82,7 +102,7 @@ double f(double x) {
         return cos(3 * x) / pow(1 + x * x, 0.2);
     else
         return log(x * x + 2 * x + 2) *
-        (4 * x * x * x - x * x + 3 * x - 2);
+               (4 * x * x * x - x * x + 3 * x - 2);
 }
 
 double solve(double y, double a, double b, double eps) {
@@ -110,17 +130,18 @@ void table(double a, double b, double step) {
     printf("----------------------------------------\n");
 }
 
-void minmax(double a, double b, double step) {
-    double minx = a, maxx = a;
-    double minv = f(a), maxv = f(a);
+void minmax(double a, double b, double step,
+            double* minx, double* minv,
+            double* maxx, double* maxv) {
+
+    *minx = a;
+    *maxx = a;
+    *minv = f(a);
+    *maxv = f(a);
 
     for (double t = a; t <= b; t += step) {
         double v = f(t);
-        if (v < minv) { minv = v; minx = t; }
-        if (v > maxv) { maxv = v; maxx = t; }
+        if (v < *minv) { *minv = v; *minx = t; }
+        if (v > *maxv) { *maxv = v; *maxx = t; }
     }
-
-    printf("\nМинимум: f(%.5f) = %.6f\n", minx, minv);
-    printf("Максимум: f(%.5f) = %.6f\n", maxx, maxv);
 }
-
